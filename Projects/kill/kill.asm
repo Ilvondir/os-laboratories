f1e94d3e0a5cd3cee3291fd1562eb52e13809153
Michał Komsa <101294147+Ilvondir@users.noreply.github.com>
2023-11-14 16:01:40 +0100

    Add files via upload

---
+++ Projects/kill/kill.asm
@@ -0,0 +1,61 @@
+    [bits 64]
+
+section .data
+str0    db "Enter process id: ", 0
+str1    db "%d", 0
+str2    db "Success! Process killed!", 0xA, 0
+str3    db "Error! There is no process with this id or you dont have permissions to kill it.", 0xA, 0
+
+section .bss
+    pid resq 1
+
+section .text
+    extern printf
+    extern scanf
+    global main
+
+main:
+
+    push rbp
+    mov  rbp, rsp
+
+    mov rdi, str0       ; str0 moved to rdi as format
+    call printf
+    
+    mov  rdi, str1      ; str1 moved to rdi as format
+    mov  rsi, pid       ; adrress pid moved to rsi
+    call scanf
+    
+    pop rbp
+
+    mov rdi, [pid]      ; process id to killed
+    mov rsi, 9          ; signal (9: kill)
+    mov rax, 62         ; syscall number for kill
+    syscall
+
+    cmp rax, 0          ; compare rax with 0
+
+    push rbp
+
+    je success
+
+failure:
+    mov rdi, str3       ; str3 moved to rdi as format
+    call printf
+
+    jmp exit
+
+success:    
+    mov rdi, str2       ; str2 moved to rdi as format
+    call printf
+
+exit:
+    pop rbp
+
+    mov rax, 60         ; syscall number for exit
+    mov rdi, 0          ; returned status
+    syscall
+
+; Michal Komsa
+;   nasm -felf64 kill.asm
+;   gcc -m64 -no-pie -o kill kill.o
---
+++ Projects/kill/kill.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<signal.h>
+
+int main(int argc, char *argv[]) {
+	
+	if (argc == 1 || argc > 2) {
+		printf("One parameter expected!\n");
+		return 1;
+	}
+	
+	int len = strlen(argv[1]);
+	
+	int i;
+	for (i = 0; i < len; i++) {
+		if (!isdigit(argv[1][i])) {
+			printf("Parameter must be a number!\n");
+			return 1;
+		}
+	}
+	
+	printf("Parameter (process id): %s\n", argv[1]);
+	
+	pid_t process_id = atoi(argv[1]);
+	
+	char command[50] = "ps -p ";
+	strcat(command, argv[1]);
+	strcat(command,  " -o comm=");
+	
+	FILE *pipe = popen(command, "r");
+	
+	char process_name[100];
+	
+	fgets(process_name, 100, pipe);
+	
+	fclose(pipe);
+	
+	if (strlen(process_name) == 0) {
+		printf("There is no process with id %s.\n", argv[1]);
+		return 1;
+	}
+	else {
+		process_name[strlen(process_name) - 1] = '\0';
+		
+		int status = kill(process_id, 9);
+		
+		if (status == 0) printf("Process %s killed successfuly!\n", process_name);
+		else printf("Process %s was not killed. \nRemember about 'sudo' :)\n", process_name);
+	}
+	
+	return 0;
+}
